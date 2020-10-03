498a2ed426ff2767b49902140a779831fb5c0127
ferron gerald <opstyle1@hotmail.fr>
2020-10-03 13:07:53 +0100

    ajout de l id url page produit et recuperation de donnee de produit

--- produit.js
+++ produit.js
@@ -1,10 +1,12 @@
-function recup(callback) {
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      var response = JSON.parse(this.responseText);
-    }
-  };
-  request.open("GET", "http://localhost:3000/api/cameras/");
-  request.send();
-}
+let id = window.location.search.substring(1);
+console.log(id);
+const url = `http://localhost:3000/api/cameras/${id}`;
+const request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+  }
+};
+request.open("GET", url);
+request.send();
