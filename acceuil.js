177129ed78bb1e76f519d9f0a8c1c16221fd8fe6
ferron gerald <opstyle1@hotmail.fr>
2020-10-04 11:51:25 +0100

    affichage produit page produit et ajout option lenses efface com

--- acceuil.js
+++ acceuil.js
@@ -4,33 +4,6 @@ request.onreadystatechange = function () {
     var response = JSON.parse(this.responseText);
     console.log(response);
 
-    // for (let i = 0; i < response.length; i++) {
-    //   let descrip = ["descri1", "descri2", "descri3", "descri4", "descri5"];
-    //   let img = ["img1", "img2", "img3", "img4", "img5"];
-    //   let title = ["titre1", "titre2", "titre3", "titre4", "titre5"];
-    //   let prix = ["prix1", "prix2", "prix3", "prix4", "prix5"];
-
-    //   console.log(response[i]._id);
-
-    //   document.getElementById(descrip[i]).innerHTML = response[i].description;
-    //   document.getElementById(img[i]).setAttribute("src", response[i].imageUrl);
-    //   document.getElementById(title[i]).innerHTML =
-    //     "Appareil photo : " + response[i].name;
-    //   document.getElementById(prix[i]).innerHTML =
-    //     "Prix : " + response[i].price + " euro";
-    // }
-
-    /*let produit = document.getElementById("produit");
-    var carte = `<div class="card border-primary shadow"><img class="card-img-top" src="" alt="logo-job" height="355px"/><div class="card-body"><h4 class="card-title">Appareil photo :</h4><h6>Description : </h6><p class="card-text"></p><h6 class="pb-1">Prix : euro</h6><a class="btn btn-info" href="produit.html" role="button">Voir le produit</a></div></div>`;
-    /* var carte = `<div class="card border-primary shadow"><img class="card-img-top" src="${response[i].imageUrl}" alt="logo-job" height="355px"/><div class="card-body"><h4 class="card-title">Appareil photo : ${response[i].name}</h4><h6>Description : </h6><p class="card-text">${response[i].description}</p><h6 class="pb-1">Prix : ${response[i].price} euro</h6><a class="btn btn-info" href="produit.html" role="button">Voir le produit</a></div></div>`;
-     */
-    // console.log(response[i]._id);
-    // document.getElementById(descrip[i]).innerHTML = response[i].description;
-    // document.getElementById(img[i]).setAttribute("src", response[i].imageUrl);
-    // document.getElementById(title[i]).innerHTML =
-    //   "Appareil photo : " + response[i].name;
-    // document.getElementById(prix[i]).innerHTML =
-    //   "Prix : " + response[i].price + " euro";
     var a = document.getElementById("carte");
     let produit = document.getElementById("produit");
 
@@ -54,7 +27,7 @@ request.onreadystatechange = function () {
       console.log("http://localhost:3000/api/cameras/" + response[i]._id);
 
       let page2 = document.querySelectorAll("#page2");
-      page2[i].href = "produit.html?" + response[i]._id;
+      page2[i].href = "produit.html?/id=" + response[i]._id;
       console.log(page2);
     }
   }
