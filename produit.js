bc55346e2d1058daacd4050dc74a32f6a2bb75c9
ferron gerald <opstyle1@hotmail.fr>
2020-10-07 15:45:34 +0100

    modifier panier preparation ajout panier localstorage boucle lenses

--- produit.js
+++ produit.js
@@ -12,22 +12,63 @@ request.onreadystatechange = function () {
     let imgP = document.getElementById("img_produit");
     let titleP = document.getElementById("title_produit");
     let prixP = document.getElementById("prix_produit");
-    let option1 = document.getElementById("lent1");
-    let option2 = document.getElementById("lent2");
+    let lentilles = document.getElementById("lentilles");
+    let panier = document.getElementById("panier");
 
     descripP.textContent = response.description;
     imgP.setAttribute("src", response.imageUrl);
     titleP.textContent = "Appareil photo : " + response.name;
     prixP.textContent = "Prix : " + response.price + " euro";
 
-    if (response.lenses[1] !== undefined) {
-      option1.textContent = response.lenses[0];
-      option2.textContent = response.lenses[1];
-    } else {
-      option1.textContent = response.lenses[0];
-      option2.remove(option2);
+    for (i = 0; i < response.lenses.length; i++) {
+      let option = document.createElement("option");
+      lentilles.appendChild(option);
+      option.textContent = response.lenses[i];
+      option.value = response.lenses[i];
     }
+
+    function clic() {
+      console.log("Clic !");
+      console.log(
+        response.name,
+        lentilles.value,
+        response.price,
+        response.imageUrl
+      );
+
+      let tab = {
+        name: response.name,
+        lentille: lentilles.value,
+        prix: response.price,
+        img: response.imageUrl,
+      };
+      let tabLine = JSON.stringify(tab);
+      localStorage.setItem("object", tabLine);
+
+      console.log(localStorage.getItem("object"));
+
+      var line = localStorage.getItem("object");
+      var objectJs = JSON.parse(line);
+
+      console.log(objectJs);
+      console.log(line.length);
+      console.log(objectJs.name);
+      console.log(localStorage.length);
+    }
+    panier.addEventListener("click", clic);
   }
 };
 request.open("GET", url);
 request.send();
+/*
+lentilles.addEventListener("change", function () {
+  console.log(lentilles.value);
+});*/
+/*
+console.log(localStorage.length);
+localStorage.setItem("timer", 12);
+console.log(localStorage.getItem("timer"));
+
+localStorage.setItem("name", "salut");
+console.log(localStorage.getItem("name"));
+*/
