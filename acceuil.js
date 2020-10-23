b1b78732e24c2278d537509df03cd54d8bf881cb
ferron gerald <opstyle1@hotmail.fr>
2020-10-23 15:03:57 +0100

    modif apres soutenance conseil de l evaluateur sur 3 points promise fonctions le parametre oublier et changement pratique des id remplacer par des classes

--- acceuil.js
+++ acceuil.js
@@ -2,8 +2,8 @@
 
 //::::::::::::::::::::::::::::::::::::: recuperation carte html et parent :::::::::::::::::::::::::::::::::::
 
-let carte = document.getElementById("carte");
-let produit = document.getElementById("produit");
+let carte = document.querySelector(".carte");
+let produit = document.querySelector(".produit");
 
 // ::::::::::::::::::::::::::::::::::::::::: function response serveur :::::::::::::::::::::::::::::::::::::::
 ajaxGet("http://localhost:3000/api/cameras")
@@ -32,11 +32,11 @@ function afficherCarte(response) {
 function rempliCarte(response) {
   // recuperation de toutes les carte cloner et leur contenu
 
-  let carteClone = document.querySelectorAll("#carte");
-  let descrip = document.querySelectorAll("#descri1");
-  let img = document.querySelectorAll("#img1");
-  let title = document.querySelectorAll("#titre1");
-  let prix = document.querySelectorAll("#prix1");
+  let carteClone = document.querySelectorAll(".carte");
+  let descrip = document.querySelectorAll(".descri1");
+  let img = document.querySelectorAll(".img1");
+  let title = document.querySelectorAll(".titre1");
+  let prix = document.querySelectorAll(".prix1");
   console.log(carteClone);
 
   // boucle ajout du contenu dans les differentes cartes et redirection sur la page produit choisi
@@ -50,7 +50,7 @@ function rempliCarte(response) {
 
     console.log("http://localhost:3000/api/cameras/" + response[i]._id);
 
-    let pageProduit = document.querySelectorAll("#page2");
+    let pageProduit = document.querySelectorAll(".page2");
     pageProduit[i].href = "produit.html?/id=" + response[i]._id;
     console.log(pageProduit);
   }
